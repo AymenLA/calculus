70da316cf6bea24a95f8acc3d20873b2ea1cc85c
LAOUINI Aymen <aymen.laouini@ymail.com>
2018-04-07 13:01:37 +0200

    working_hex_print : added a print function to show hex tables

--- include/affichage.h
+++ include/affichage.h
@@ -3,6 +3,7 @@
 
 
 uint32_t print_data(uint32_t u32_val1);
+uint32_t print_hex(uint8_t *pu8_data,uint32_t u32_len);
 uint32_t print_text(uint8_t *u8p_text,uint8_t u8_type, uint8_t u8_color);
 
 #endif /* _AFFICHAGE_H_ */
--- src/affichage.c
+++ src/affichage.c
@@ -16,7 +16,30 @@ uint32_t print_data(uint32_t u32_val1)
 	return 0;
 }
 
-uint32_t print_text(uint8_t *u8p_text,uint8_t u8_type, uint8_t u8_color)
+uint32_t print_hex(uint8_t *pu8_data,uint32_t u32_len)
+{
+	uint32_t u32_i = 0;
+
+	for(u32_i = 1; u32_i < u32_len + 1; u32_i++)
+	{
+		printf("0x%0.2x ",*(pu8_data + u32_i - 1));
+
+		if( (u32_i - ((u32_i / 8) * 8)) == 0)
+		{
+			printf("\n");
+		}
+
+		else if( (u32_i - ((u32_i / 4) * 4)) == 0)
+		{
+			printf("\t");
+		}
+
+	}
+	printf("\n");
+
+	return 0;	
+}
+uint32_t print_(uint8_t *u8p_text,uint8_t u8_type, uint8_t u8_color)
 {
 	return 0;
 }
